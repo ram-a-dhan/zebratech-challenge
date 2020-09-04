e93d537056c0e6df23726e2fd9d13f7e2352f9b5
ram-a-dhan <ram.adhan@outlook.com>
2020-09-04 09:44:03 +0700

    basic layout done;

--- src/pages/FoodConsumption/index.js
+++ src/pages/FoodConsumption/index.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Grid, Box, Paper, Typography } from '@material-ui/core';
 
-const FoodConsumption = () => (
-  <div>Work in progress</div>
-)
+const FoodConsumption = () => {
+  return (
+    <Grid spacing={3} container>
+      <Grid xs={12} item>
+        <Box component={Paper} height="100%">
+          <Box p={3}>
+            <Typography variant="h6" gutterBottom>
+              Daily Food Consumption
+            </Typography>
+            
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
+  )
+}
 
 export default FoodConsumption
